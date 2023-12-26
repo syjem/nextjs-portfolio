9822dc94953a09a38121bf6f173f009fc6a27be1
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-12-26 17:36:03 +0800

    fix width on mobile device

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Socials from './socials';
 
 const Footer = () => {
   return (
-    <footer className="py-8 w-full max-w-5xl mx-auto mt-4 sm:mt-20 scroll-mt-24 px-4 flex flex-col gap-2 sm:flex-row shrink-0 items-center md:px-6 border-t">
+    <footer className="py-8 w-[90%] max-w-5xl mx-auto mt-4 sm:mt-20 scroll-mt-24 px-4 flex flex-col gap-2 sm:flex-row shrink-0 items-center md:px-6 border-t">
       <p className="text-xs text-gray-500 dark:text-gray-400">
         © Jemuel Repoylo. All rights reserved.
       </p>
